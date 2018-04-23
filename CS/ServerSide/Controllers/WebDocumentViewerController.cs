8e8931172f82d100381fee9648db1a6ffa88e31d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ServerSide/Controllers/WebDocumentViewerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using DevExpress.Web.Mvc.Controllers;
+using System.Web.Mvc;
+using DevExpress.XtraReports.Web.Extensions;
+
+namespace ServerSide.Controllers
+{
+    public class WebDocumentViewerController : WebDocumentViewerApiController {
+        public override ActionResult Invoke() {
+            var result = base.Invoke();
+            // Allow cross-domain requests.
+            Response.AppendHeader("Access-Control-Allow-Origin", "*");
+            return result;
+        }
+
+        [HttpPost]
+        public ActionResult GetReports() {
+            Response.AppendHeader("Access-Control-Allow-Origin", "*");
+            var result = new JsonResult {
+                Data = ReportStorageWebService.GetUrls().ToArray()
+            };
+            return result;
+        }
+    }
+}
---
+++ VB/ServerSide/Controllers/WebDocumentViewerController.vb
@@ -0,0 +1,24 @@
+﻿Imports DevExpress.Web.Mvc.Controllers
+Imports DevExpress.XtraReports.Web.Extensions
+Imports System.Linq
+Imports System.Web.Mvc
+
+Namespace ServerSide.Controllers
+    Public Class WebDocumentViewerController
+        Inherits WebDocumentViewerApiController
+
+        Public Overrides Function Invoke() As ActionResult
+            Dim result = MyBase.Invoke()
+            ' Allow cross-domain requests.
+            Response.AppendHeader("Access-Control-Allow-Origin", "*")
+            Return result
+        End Function
+
+        <HttpPost> _
+        Public Function GetReports() As ActionResult
+            Response.AppendHeader("Access-Control-Allow-Origin", "*")
+            Dim result = New JsonResult With {.Data = ReportStorageWebService.GetUrls().ToArray()}
+            Return result
+        End Function
+    End Class
+End Namespace
